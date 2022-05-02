b76883a2579aab07eeaf0a30fd565489f575558e
Joanna Liana <joanna_liana@outlook.com>
2022-05-02 14:27:52 +0200

    Refactor printing

--- cli/cli.go
+++ cli/cli.go
@@ -49,7 +49,7 @@ func (cli AliasCLI) parseArgs() (string, string, error) {
 			errorString = "missing command"
 		}
 
-		fmt.Fprintln(cli.printer, "Error:\t", errorString, "\nUsage:\t alias-it <alias_name> <command_name>")
+		cli.println("Error:\t", errorString, "\nUsage:\t alias-it <alias_name> <command_name>")
 
 		return "", "", errors.New("insufficient args")
 	}
@@ -58,8 +58,8 @@ func (cli AliasCLI) parseArgs() (string, string, error) {
 	commandParts := os.Args[2:]
 	command := strings.Join(commandParts, " ")
 
-	fmt.Fprintf(cli.printer, "🏡 Alias name:\t%s\n", aliasName)
-	fmt.Fprintf(cli.printer, "💻 Command:\t%s\n", command)
+	cli.printf("🏡 Alias name:\t%s\n", aliasName)
+	cli.printf("💻 Command:\t%s\n", command)
 
 	return command, aliasName, nil
 }
@@ -68,7 +68,7 @@ func (cli AliasCLI) getShellConfigPath(homeDirResolver HomeDirResolver) string {
 	homeDir, err := homeDirResolver()
 
 	if err != nil {
-		fmt.Fprintln(cli.printer, err)
+		cli.println(err)
 		panic(err)
 	}
 
@@ -81,13 +81,13 @@ func (cli AliasCLI) appendToShellConfig(shellConfigPath string, toAppend string)
 	f, err := os.OpenFile(shellConfigPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 
 	if err != nil {
-		fmt.Fprintln(cli.printer, err)
+		cli.println(err)
 	}
 
 	defer f.Close()
 
 	if _, err := f.WriteString(toAppend); err != nil {
-		fmt.Fprintln(cli.printer, err)
+		cli.println(err)
 	}
 }
 
@@ -96,7 +96,15 @@ func (cli AliasCLI) addAlias(name, command, shellConfigPath string) {
 
 	cli.appendToShellConfig(shellConfigPath, aliasString)
 
-	fmt.Fprintf(cli.printer, "\nAdded alias:%v\n\n", aliasString)
-	fmt.Fprintln(cli.printer, "Hint: to prevent variable expansion, remember about prepending $ with a slash, e.g. $PWD -> \\$PWD")
-	fmt.Fprintln(cli.printer, "\n👉 Remember to run \"source ~/.zshrc\" or open a new terminal tab to start using your alias")
+	cli.printf("\nAdded alias:%v\n\n", aliasString)
+	cli.println("Hint: to prevent variable expansion, remember about prepending $ with a slash, e.g. $PWD -> \\$PWD")
+	cli.println("\n👉 Remember to run \"source ~/.zshrc\" or open a new terminal tab to start using your alias")
+}
+
+func (cli AliasCLI) printf(format string, toPrint ...any) {
+	fmt.Fprintf(cli.printer, format, toPrint...)
+}
+
+func (cli AliasCLI) println(toPrint ...any) {
+	fmt.Fprintln(cli.printer, toPrint...)
 }
