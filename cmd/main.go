55b259d163bc79048ccc0ae28db6962ffb3e9ba5
Joanna Liana <joanna_liana@outlook.com>
2022-07-16 17:27:15 +0200

    Make lack of Windows support explicit

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"alias_it/cli"
+	"fmt"
 	"os"
+	"runtime"
+	"strings"
 )
 
 func main() {
+	if strings.HasPrefix(runtime.GOOS, "windows") {
+		fmt.Println("Apologies, you cannot use this app on Windows. Try WSL!")
+
+		os.Exit(1)
+	}
+
 	cli := cli.New(os.Stdout, os.UserHomeDir)
 
 	cli.Add()
